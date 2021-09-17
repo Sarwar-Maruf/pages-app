659a1047732ff8896a0606d6eebb72f81d83bed1
Sarwar-Maruf <sarwar.maruf9@gmail.com>
2021-09-17 10:34:26 +0600

    New update for heroku deplyment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '509_obxj#=f)jj1yvy-dqtv&&mumaso03r)evo*ng^x^+&am6-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
